0624a191bdcec5cd38288147538875c6fb790a24
Nika Kvaratskhelia <nikakvaracxeliaa@gmail.com>
2023-11-18 21:49:29 +0400

    usestate

--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -1,117 +1,101 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import TaskItem from "./TaskItem";
 import TodoTaskList from "./TodoTaskList";
 
-class TaskLists extends Component {
-  state = {
-    inputValue: "",
-    tasks: [],
-    completedTasks: [],
-  };
+const TaskLists = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [completedTasks, setCompletedTasks] = useState([]);
 
-  onChange = (event) => {
+  const onChange = (event) => {
     const value = event.target.value;
-    this.setState({
-      inputValue: value,
-    });
+    setInputValue(value);
   };
 
-  addTask = (event) => {
+  const addTask = (event) => {
     event.preventDefault();
 
     const task = {
-      id: this.state.tasks.length + 1,
-      task: this.state.inputValue,
+      id: tasks.length + 1,
+      task: inputValue,
     };
 
-    this.setState({
-      tasks: [...this.state.tasks, task],
-      inputValue: "", 
-    });
+    setTasks([...tasks, task]);
+    setInputValue("");
   };
 
-  deleteTask = (id) => {
-    const updatedTasks = this.state.tasks.filter((task) => task.id !== id);
-    const updatedCompletedTasks = this.state.completedTasks.filter(
+  const deleteTask = (id) => {
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+    const updatedCompletedTasks = completedTasks.filter(
       (task) => task.id !== id
     );
-    this.setState({
-      tasks: updatedTasks,
-      completedTasks: updatedCompletedTasks,
-    });
+
+    setTasks(updatedTasks);
+    setCompletedTasks(updatedCompletedTasks);
   };
 
-  undoTask = (id) => {
-    const taskToUndo = this.state.completedTasks.find((task) => task.id === id);
+  const undoTask = (id) => {
+    const taskToUndo = completedTasks.find((task) => task.id === id);
+
     if (taskToUndo) {
-      const updatedCompletedTasks = this.state.completedTasks.filter(
+      const updatedCompletedTasks = completedTasks.filter(
         (task) => task.id !== id
       );
-      this.setState({
-        tasks: [...this.state.tasks, taskToUndo],
-        completedTasks: updatedCompletedTasks,
-      });
+
+      setTasks([...tasks, taskToUndo]);
+      setCompletedTasks(updatedCompletedTasks);
     }
   };
 
-  completeTask = (id) => {
-    const taskToComplete = this.state.tasks.find((task) => task.id === id);
+  const completeTask = (id) => {
+    const taskToComplete = tasks.find((task) => task.id === id);
+
     if (taskToComplete) {
-      const updatedTasks = this.state.tasks.filter((task) => task.id !== id);
-      this.setState({
-        id: this.state.tasks.length + 1,
-        tasks: updatedTasks,
-        completedTasks: [...this.state.completedTasks, taskToComplete],
-      });
+      const updatedTasks = tasks.filter((task) => task.id !== id);
+
+      setTasks(updatedTasks);
+      setCompletedTasks([...completedTasks, taskToComplete]);
     }
-    console.log("sheqmna");
   };
 
-  render() {
-    return (
-      <div className="App">
-        <form onSubmit={this.addTask}>
-          <input
-            type="text"
-            placeholder="enter your task"
-            onChange={this.onChange}
-            value={this.state.inputValue}
-          />
-          <button type="submit">submit</button>
-        </form>
-        <div className="container">
-            <div className="firstList">
-              <h2> შესასრულებელი</h2>
-              {this.state.tasks.map((tsk) => (
-                <TaskItem
-                  key={tsk.id}
-                  task={tsk.task}
-                  id={tsk.id}
-                  onComplete={this.completeTask}
-                />
-              ))}
-            </div>
-          <div className="secondList">
-            <h2>შესრულებული</h2>
-            {this.state.completedTasks.map((e) => (
-              <TodoTaskList
-                key={e.id}
-                task={e.task}
-                id={e.id}
-                action={this.deleteTask}
-                action2={this.undoTask}
-              />
-            ))}
-          </div>
+  return (
+    <div className="App">
+      <form onSubmit={addTask}>
+        <input
+          type="text"
+          placeholder="enter your task"
+          onChange={onChange}
+          value={inputValue}
+        />
+        <button type="submit">submit</button>
+      </form>
+      <div className="container">
+        <div className="firstList">
+          <h2>შესასრულებელი</h2>
+          {tasks.map((tsk) => (
+            <TaskItem
+              key={tsk.id}
+              task={tsk.task}
+              id={tsk.id}
+              onComplete={completeTask}
+            />
+          ))}
+        </div>
+        <div className="secondList">
+          <h2>შესრულებული</h2>
+          {completedTasks.map((e) => (
+            <TodoTaskList
+              key={e.id}
+              task={e.task}
+              id={e.id}
+              action={deleteTask}
+              action2={undoTask}
+            />
+          ))}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default TaskLists;
-
-
-
-
-
